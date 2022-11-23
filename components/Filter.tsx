97afeebade4bc7574136f2e377e511ca1d33eb1c
Quang Tran <wooden_box@hotmail.com>
2022-11-23 23:29:43 +1100

    Main view for mobile done.

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,9 +1,10 @@
 import { Ruge_Boogie } from '@next/font/google';
 import Image from 'next/image';
-import {Dispatch, SetStateAction} from 'react';
+import {Dispatch, SetStateAction, useLayoutEffect, useState} from 'react';
 import Select from 'react-select';
 import magnifyingGlassLight from '../public/images/magnifying-glass-gray.svg';
 import magnifyingGlassDark from '../public/images/magnifying-glass-white.svg';
+import {breakPt} from '../resources/constants';
 import {mulish} from '../resources/fonts';
 import {Region} from '../resources/types';
 import style from '../styles/Filter.module.scss';
@@ -25,6 +26,23 @@ export default function Filter({isDark, region, setRegion, searchTerm, setSearch
   searchTerm: string,
   setSearchTerm: Dispatch<SetStateAction<string>>}
 ) {
+  const [winWidth, setWinWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    setWinWidth(window.innerWidth);
+
+    window.addEventListener(
+      'resize',
+      () => setWinWidth(window.innerWidth)
+    );
+
+    return () => {
+      window.removeEventListener(
+        'resize',
+        () => setWinWidth(window.innerWidth)
+      );
+    }
+  }, [])
 
   return (
     <div
@@ -72,21 +90,26 @@ export default function Filter({isDark, region, setRegion, searchTerm, setSearch
         styles={{
           control: (baseStyles, state) => ({
             ...baseStyles,
-            height: 56,
+            height: winWidth > breakPt ? 56 : 48,
             width: 200,
             border: 'none',
             paddingLeft: 15,
-            fontSize: 14,
-            fontWeight: 600,
+            fontSize: winWidth > breakPt ? 14 : 12,
+            fontWeight: 400,
             backgroundColor: isDark ? 'rgb(43, 55, 67)' : 'white',
             borderRadius: 3
-            // color: isDark ? 'white' : 'black',
-            // outline: 'none',
           }),
-          singleValue: (base) => ({
-            ...base,
+          singleValue: (baseStyles) => ({
+            ...baseStyles,
             color: isDark ? 'white' : 'rgb(20, 20, 20)',
           }),
+          option: (baseStyles) => ({
+            ...baseStyles,
+            paddingLeft: 25,
+            fontSize: winWidth > breakPt ? 14 : 12,
+
+          })
+
         }}
       />
     </div>
---
+++ resources/constants.ts
@@ -0,0 +1,1 @@
+export const breakPt = 720;
